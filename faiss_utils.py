ce7b531968ef211b7bf2f8c5cc22898f7a12afa4
Leegwangwoon <leegwangwoon@naver.com>
2025-03-30 13:16:50 +0900

    Refactor: delete unused files

--- faiss_utils.py
+++
@@ -1,9 +0,0 @@
-
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-
-def load_faiss_vectorstore():
-    embedding_model = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large")
-    vectorstore = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
-    return retriever
